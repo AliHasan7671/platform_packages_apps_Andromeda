d6d89b116d8ad423a8f3867d7406e0e7940dbae5
bigrushdog <randall.rushing@gmail.com>
2018-08-21 10:24:06 +0800

    AndormedaSettings: Add navigation bar visibility control [2/2]
    included Protect against SystemUI FC when toggling navbar too fast by ezio84
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>
    Change-Id: I312027c2776bacc38cf20adf70096ec87642e599
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- src/com/aosap/settings/fragments/NavbarSettings.java
+++ src/com/aosap/settings/fragments/NavbarSettings.java
@@ -13,52 +13,74 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.aosap.settings.fragments;
 
-import java.util.ArrayList;
-
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.UserHandle;
+import android.provider.Settings;
+
 import androidx.preference.ListPreference;
 import androidx.preference.SwitchPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import android.provider.SearchIndexableResource;
-import android.provider.Settings;
 
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settingslib.search.Indexable;
-import com.android.settingslib.search.SearchIndexable;
-import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.hwkeys.ActionUtils;
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 
-import java.util.List;
-import java.util.ArrayList;
+public class NavbarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
 
-@SearchIndexable
-public class NavbarSettings extends SettingsPreferenceFragment implements 
-		OnPreferenceChangeListener, Indexable {
+    private static final String NAVBAR_VISIBILITY = "navbar_visibility";
+
+    private SwitchPreference mNavbarVisibility;
+
+    private boolean mIsNavSwitchingMode = false;
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.aosap_settings_navigation);
 
+        mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
+
+        boolean showing = Settings.System.getInt(getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR,
+                ActionUtils.hasNavbarByDefault(getActivity()) ? 1 : 0) != 0;
+        updateBarVisibleAndUpdatePrefs(showing);
+        mNavbarVisibility.setOnPreferenceChangeListener(this);
+
+        mHandler = new Handler();
+    }
+
+    private void updateBarVisibleAndUpdatePrefs(boolean showing) {
+        mNavbarVisibility.setChecked(showing);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-
+        if (preference.equals(mNavbarVisibility)) {
+            if (mIsNavSwitchingMode) {
+                return false;
+            }
+            mIsNavSwitchingMode = true;
+            boolean showing = ((Boolean)newValue);
+            Settings.System.putInt(getContentResolver(), Settings.System.FORCE_SHOW_NAVBAR,
+                    showing ? 1 : 0);
+            updateBarVisibleAndUpdatePrefs(showing);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsNavSwitchingMode = false;
+                }
+            }, 1500);
+            return true;
+        }
         return false;
     }
 
@@ -66,25 +88,4 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.AOSAP;
     }
-	
-	public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-		new BaseSearchIndexProvider() {
-			@Override
-			public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-					boolean enabled) {
-				ArrayList<SearchIndexableResource> result =
-						new ArrayList<SearchIndexableResource>();
-
-				SearchIndexableResource sir = new SearchIndexableResource(context);
-				sir.xmlResId = R.xml.aosap_settings_navigation;
-				result.add(sir);
-				return result;
-			}
-
-			@Override
-			public List<String> getNonIndexableKeys(Context context) {
-				List<String> keys = super.getNonIndexableKeys(context);
-				return keys;
-			}
-    };
 }
