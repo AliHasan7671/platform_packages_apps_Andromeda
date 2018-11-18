1c90b9875f3a4440ec1524aad76051238c34f904
ezio84 <brabus84@gmail.com>
2018-11-18 17:44:03 +0100

    Restore less annoying notifs options
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- src/com/aosap/settings/fragments/VolumeRockerSettings.java
+++ src/com/aosap/settings/fragments/VolumeRockerSettings.java
@@ -15,6 +15,8 @@ import com.android.settings.SettingsPreferenceFragment;
 public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private ListPreference mQuickPulldown;
+    private ListPreference mAnnoyingNotification
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -22,11 +24,25 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.settings_volume);
 
+        mAnnoyingNotification = (ListPreference) findPreference("less_notification_sounds");
+        mAnnoyingNotification.setOnPreferenceChangeListener(this);
+        int threshold = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD,
+                30000, UserHandle.USER_CURRENT);
+        mAnnoyingNotification.setValue(String.valueOf(threshold));
+
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    ContentResolver resolver = getActivity().getContentResolver();
 
+	if (preference.equals(mAnnoyingNotification)) {
+            int mode = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, mode, UserHandle.USER_CURRENT);
+            return true;
+        }
         return false;
     }
 
