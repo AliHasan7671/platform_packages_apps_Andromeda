4f332447d844b9ce5e79a65b11658d3edc654d2c
Pranav Vashi <neobuddy89@gmail.com>
2020-02-03 01:42:55 +0530

    QS panel opacity [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>
    Change-Id: Ic5a1b84dc991d8405a279c9ef132516b2ffa460a
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- src/com/aosap/settings/fragments/QuickSettings.java
+++ src/com/aosap/settings/fragments/QuickSettings.java
@@ -52,6 +52,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         ContentResolver resolver = mContext.getContentResolver();
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_TILE_TITLE_VISIBILITY, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_PANEL_BG_ALPHA, 255, UserHandle.USER_CURRENT);
         CustomHeader.reset(mContext);
     }
 
